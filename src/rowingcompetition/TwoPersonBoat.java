60530faa43fd162ff04cfa298dc7fbdab18506a6
akless <andre.kless@web.de>
2020-11-19 18:38:22 +0100

    minor change

--- src/rowingcompetition/TwoPersonBoat.java
+++ src/rowingcompetition/TwoPersonBoat.java
@@ -1,5 +1,10 @@
 package rowingcompetition;
 
+/**
+ * simulation of a 2-person boat in a rowing competition
+ * @author André Kless <andre.kless@web.de>
+ * @version 1.0.0
+ */
 public class TwoPersonBoat extends Boat {
 
 	public TwoPersonBoat() {
