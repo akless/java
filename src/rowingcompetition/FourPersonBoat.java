0a743a2d839baddc60f2a8f3a895da678e5b1be7
akless <andre.kless@web.de>
2020-11-19 18:38:41 +0100

    minor change

--- src/rowingcompetition/FourPersonBoat.java
+++ src/rowingcompetition/FourPersonBoat.java
@@ -1,5 +1,10 @@
 package rowingcompetition;
 
+/**
+ * simulation of a 4-person boat in a rowing competition
+ * @author André Kless <andre.kless@web.de>
+ * @version 1.0.0
+ */
 public class FourPersonBoat extends Boat {
 
 	public FourPersonBoat() {
